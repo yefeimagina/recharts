61387f6080f60a6eaa8457d27d81950e3e1eb03d
yefeimagina <31589795+yefeimagina@users.noreply.github.com>
2018-07-30 15:50:53 +0800

    Update eLine.R

--- R/eLine.R
+++ R/eLine.R
@@ -316,124 +316,17 @@ eLine = function(dat, xvar=NULL, yvar=NULL, series=NULL, size = NULL, horiz = FA
 #'  eArea(dat)
 #'
 #' 
-eArea = function(dat, xvar=NULL, yvar=NULL, series=NULL, size = NULL, horiz = FALSE, stack="SUM",
-	theme = "default", title = NULL, subtitle = NULL, title.x = "center", title.y = "top", 
-	legend = TRUE, legend.x = "left", legend.y= "top", legend.orient="horizontal", 
-	toolbox = TRUE, toolbox.orient = "horizontal", toolbox.x = "right", toolbox.y = "top", 
-	dataView = TRUE, readOnly = FALSE, mark=TRUE, dataZoom=FALSE, magicType=TRUE,
-	tooltip = TRUE, tooltip.trigger="item", formatter="", axis.scale=FALSE,
-	axis.line=TRUE, axis.tick=FALSE, axis.lable=TRUE, axis.splitLine=TRUE, axis.splitArea=FALSE, axis.boundaryGap=TRUE,
-	xlab=TRUE, xlab.type="category", xlab.data=NULL, xlab.position="bottom",
-	xlab.name = "", xlab.namePosition="start", xlim=NULL,
-	ylab=TRUE, ylab.type="value", ylab.data=NULL, ylab.position="left", 
-	ylab.name = "", ylab.namePosition="start", ylim=NULL,
-	calculable=TRUE, showLabel=TRUE, opt = list()) 
+eArea = function(size = NULL) 
 {
 	
-	xlabName = autoArgLabel(xvar, deparse(substitute(xvar)))
-	ylabName = autoArgLabel(yvar, deparse(substitute(yvar)))
-	seriesName = autoArgLabel(series, deparse(substitute(series)))
-
-	xvar = evalFormula(xvar, dat)
 	
-	yvar = evalFormula(yvar, dat)
-
-	series = as.factor(evalFormula(series, dat))
-	# if series is null, we will use the xvar and yvar to construct the area plot..
-	if(is.null(xvar) & is.null(yvar) & !is.factor(dat)){
-		# Mode 1. use default data.frame as input...
-		plotData = as.data.frame(dat, stringsAsFactor=F)
-	}else if(!is.null(xvar) & !is.null(yvar) & !is.null(series)){
-		#print("Mode1")
-		# Mode 2. all of xvar, yvar and series are valid...
-		xvarArray = unique(as.character(xvar))
-		seriesArray = unique(as.character(series))
-		dataMatrix = xtabs(as.formula(paste0(ylabName, "~", xlabName , "+",  seriesName)), dat)
-		plotData <- as.data.frame.matrix(dataMatrix[xvarArray,seriesArray])
-	}else if(!is.null(xvar) & !is.null(yvar) & is.null(series)){
-		# Mode 3. format dat with only x and y variable.
-		plotData = data.frame(val = yvar)
-		colnames(plotData) = ylabName
-		rownames(plotData) = xvar
-	}else if(is.null(xvar) & is.null(yvar) & is.factor(dat)){
-		# Mode 4. factor
-		tempD = as.data.frame(table(dat))
-		plotData = data.frame(val = tempD[,"Freq"])
-		colnames(plotData) = "Frequency"
-		rownames(plotData) = tempD[,1]
-	}
-
-	
-	# option$title format.
-	opt$title = tilteSet(title = title, subtitle=subtitle,
-			title.x = title.x, title.y = title.y)
-	
-	opt$calculable = calculableSet(calculable = calculable)
-	opt$theme = themeSet(theme = theme)
-	
-	# opt$tooltip format, not open to user now.
-	opt$tooltip = tooltipSet( tooltip=tooltip,trigger=tooltip.trigger,
-			formatter = "", islandFormatter="")
-	
-	opt$toolbox = toolboxSet(toolbox=toolbox, toolbox.x=toolbox.x, toolbox.y=toolbox.y, orient=toolbox.orient,
-				dataView=dataView, mark=mark, dataZoom = dataZoom, magicType = magicType, restore = TRUE, readOnly = readOnly,
-				saveAsImage=TRUE)
-
-				
-	opt$legend = legendSet( show=legend, data=colnames(plotData), legend.x=legend.x, legend.y=legend.y, orient=legend.orient)
-	
-	if(match.arg(xlab.type, c("category" , "value")) == "category" & is.null(xlab.data)){
-		xlab.data = rownames(plotData)
-	}
-	if(match.arg(ylab.type, c("category" , "value")) == "category" & is.null(ylab.data)){
-		ylab.data = colnames(plotData)
-	}
-	opt$xAxis = xAxisSet(axisShow=xlab, type=xlab.type, data=xlab.data, position=xlab.position,
-				labelName=xlab.name, label.namePosition=xlab.namePosition, lim=xlim,
-				axisLine=axis.line, axisTick=axis.tick, axisLable=axis.lable, splitLine=axis.splitLine, 
-				splitArea=axis.splitArea, boundaryGap=axis.boundaryGap, scale=axis.scale)	
-	
-	opt$yAxis = yAxisSet(axisShow=ylab, type=ylab.type, data=ylab.data, position=ylab.position,
-				labelName=ylab.name, label.namePosition=ylab.namePosition, lim=ylim,
-				axisLine=axis.line, axisTick=axis.tick, axisLable=axis.lable, splitLine=axis.splitLine, 
-				splitArea=axis.splitArea, boundaryGap=axis.boundaryGap, scale=axis.scale)
-
-	# data set...
-	opt$series =  vector("list", ncol(plotData))
-
-    for(i in 1:ncol(plotData)) {
-        if(is.null(opt$series[[i]]$type)) {
-            opt$series[[i]]$type = 'line'
-        }
-
-        if(is.null(opt$series[[i]]$name)) {
-            opt$series[[i]]$name = colnames(plotData)[i]
-        } else {
-            warning('You can set series:name with dat.')
-        }
-        
-        if(is.null(opt$series[[i]]$data)) {
-            opt$series[[i]]$data = unnames(plotData[,i])
-        } else {
-            warning('You can set series:data with dat.')
-        }
-		
-		if(is.null(opt$series[[i]]$stack)) {
-            opt$series[[i]]$stack = 'SUM'
-        }
-
-        if(is.null(opt$series[[i]]$itemStyle$normal$areaStyle$type)) {
-            opt$series[[i]]$itemStyle$normal$areaStyle$type = 'default'
-        }
-    }
-    
-
-	if(horiz==TRUE) {
-		tmp = opt$xAxis
-		opt$xAxis = opt$yAxis
-		opt$yAxis = tmp
-	}
-	
+opt = list(
+ xAxis3D = list(type = "value"),
+ yAxis3D = list(type = "value"),
+ zAxis3D = list(type = "value"),
+ series = dd
+  
+)
 	opt$size = size
 	
 	chart = htmlwidgets::createWidget(
